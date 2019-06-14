869c27026741ed3ada51b2c626f5db2261f89d9f
zegabr <jgsp2@cin.ufpe.br>
2019-06-14 09:12:00 -0300

    bug consertado

--- bezierjs/getbezier.js
+++
@@ -1,8 +0,0 @@
-function getBezierXY(t, sx, sy, cp1x, cp1y, cp2x, cp2y, ex, ey) {//4 pontos, bezier cuubico
-  return {[
-    x: Math.pow(1-t,3) * sx + 3 * t * Math.pow(1 - t, 2) * cp1x 
-      + 3 * t * t * (1 - t) * cp2x + t * t * t * ex,
-    y: Math.pow(1-t,3) * sy + 3 * t * Math.pow(1 - t, 2) * cp1y 
-      + 3 * t * t * (1 - t) * cp2y + t * t * t * ey
-  };
-}
